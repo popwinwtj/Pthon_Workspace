270163332e53fe36a17f5f4fd8de4e36c078e79f
popwinwtj <popwinwtj@hotmail.com>
2017-08-08 23:20:34 +0800

    update

---
+++ Basic/List.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+print(L[0][0])
---
+++ Basic/Loop.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+L = ['Bart', 'Lisa', 'Adam']
+for l in L :
+    print(l)
+    
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
+
